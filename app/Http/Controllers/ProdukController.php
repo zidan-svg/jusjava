1e7a0077e774fb898b59bd59e4d1fca9c4350855
zidan-svg <zidannibrashafizhfadhilah23@mhs.politala.ac.id>
2024-10-20 11:20:14 +0800

    2

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -1,46 +1,6 @@
-namespace App\Http\Controllers;
-
-use App\Models\Produk;
-use Illuminate\Http\Request;
-
-class ProdukController extends Controller
+<php
+public function index()
 {
-    public function index()
-    {
-        $produks = Produk::all();
-        return view('produks.index', compact('produks'));
-    }
-
-    public function create()
-    {
-        return view('produks.create');
-    }
-
-    public function store(Request $request)
-    {
-        Produk::create($request->all());
-        return redirect()->route('produks.index');
-    }
-
-    public function show(Produk $produk)
-    {
-        return view('produks.show', compact('produk'));
-    }
-
-    public function edit(Produk $produk)
-    {
-        return view('produks.edit', compact('produk'));
-    }
-
-    public function update(Request $request, Produk $produk)
-    {
-        $produk->update($request->all());
-        return redirect()->route('produks.index');
-    }
-
-    public function destroy(Produk $produk)
-    {
-        $produk->delete();
-        return redirect()->route('produks.index');
-    }
+    $products = Product::paginate(10); // Ambil data produk dengan paginasi
+    return view('product', compact('products')); // Mengembalikan view 'product'
 }
--- database/migrations/2024_10_14_151536_create_produks_table.php
+++ database/migrations/2024_10_14_151536_create_produks_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
+a
     /**
      * Reverse the migrations.
      */
