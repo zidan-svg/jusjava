8c277c8b8bccf73a534c135f54a67b878639bf97
annisamaulidazuhriah <annisamaulidazuhriah23@mhs.politala.ac.id>
2024-10-30 14:08:10 +0800

    perubahan transaksi show

--- resources/views/transaksis/show.blade.php
+++ resources/views/transaksis/show.blade.php
@@ -4,26 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail Transaksi</title>
+    <title>liat transaksi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
 
     <div class="container mt-5 mb-5">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <h3 class="text-center">{{ $transaksi->Tanggal_transaksi }}</h3>
+                        <h3>{{ $transaksi->Tanggal_transaksi }}</h3>
                         <hr/>
-                        <p><strong>Total Pembayaran:</strong> {{ "Rp " . number_format($transaksi->Total_pembayaran,2,',','.') }}</p>
-                        <p><strong>Nama Pembeli:</strong> {!! $transaksi->Nama_pembeli !!}</p>
-                        <p><strong>Jumlah Barang:</strong> {{ $transaksi->Jumlah_barang }}</p>
-
-                        <div class="d-flex justify-content-between mt-4">
-                            <a href="/transaksi" class="btn btn-secondary">Kembali</a>
-                            <button onclick="window.print()" class="btn btn-primary">Cetak Struk</button>
-                        </div>
+                        <p>{{ "Rp " . number_format($transaksi->Total_pembayaran,2,',','.') }}</p>
+                        <code>
+                            <p>{!! $transaksi->Nama_pembeli !!}</p>
+                        </code>
+                        <p>Jumlah barang : {{ $transaksi->Jumlah_barang }}</p>
                     </div>
                 </div>
             </div>
