e43943c5d3a30640eca87a787cc3052d89a68983
zidan-svg <zidannibrashafizhfadhilah23@mhs.politala.ac.id>
2025-01-01 11:33:13 +0800

    Fix file naming conflict in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Product; // Pastikan model Product sudah di-import
 use App\Models\transaksi;
 use App\Models\laporan;
+use App\Models\Barangmasuk;
 use App\Models\Barangkeluar;
+use App\Models\Barang;
 
 class DashboardController extends Controller
 {
@@ -14,8 +16,9 @@ class DashboardController extends Controller
         $products = Product::all();
         $transaksis = Transaksi::all();
         $laporans = Laporan::all();
+        $barangmasuks = Barangmasuk::all();
         $barangkeluars = BarangKeluar::all();  // Assuming BarangKeluar is the correct model
-    
-        return view('dashboard', compact('products', 'transaksis', 'laporans', 'barangkeluars'));
+        $barangs = Barang::all();
+        return view('dashboard', compact('products', 'transaksis', 'laporans', 'barangmasuks', 'barangkeluars', 'barangs'));
     }
-}    
+}
